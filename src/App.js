e74be71939244e8e3662dcc18ff9b5ed0cedc92d
imlakshaychauhan <imlakshaychauhan@gmail.com>
2023-01-04 20:23:24 +0530

    Colouring Letters

--- src/App.js
+++ src/App.js
@@ -8,10 +8,31 @@ export const AppContext = createContext();
 
 const App = () => {
   const [board, setBoard] = useState(defaultBoard);
+  const [currAttempt, setCurrAttempt] = useState({attempt: 0, lettPos: 0});
+  const correctWord = "RIGHT"
+  const onSelectLetter = (keyVal) => {
+    if(currAttempt.lettPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.lettPos] = keyVal;
+    setBoard(newBoard);
+    setCurrAttempt({...currAttempt, lettPos: currAttempt.lettPos + 1});
+  }
+  const onDelete = () => {
+    if(currAttempt.lettPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.lettPos - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({...currAttempt, lettPos: currAttempt.lettPos - 1});
+  }
+  const onEnter = () => {
+    if(currAttempt.lettPos !== 5) return;
+    setCurrAttempt({ attempt: currAttempt.attempt + 1, lettPos: 0});
+  }
+
   return (
     <div className="App">
       <nav> <h1>WORDLE</h1> </nav>
-      <AppContext.Provider value={{board, setBoard}}>
+      <AppContext.Provider value={{board, setBoard, currAttempt, setCurrAttempt, onSelectLetter, onEnter, onDelete, correctWord}}>
         <div className='game' >
           <Board />
           <Keyboard />
--- src/components/Key.js
+++ src/components/Key.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../App';
 
 const Key = ({keyVal, bigKey}) => {
+  const { onSelectLetter, onEnter, onDelete } = useContext(AppContext);
+  const selectLetter = () => {
+    if(keyVal === "ENTER")
+      onEnter();
+    else if(keyVal === "DELETE")
+      onDelete();
+    else
+      onSelectLetter(keyVal);
+  }
   return (
-    <div className='key' id={bigKey && "big"} >{keyVal}</div>
+    <div className='key' id={bigKey && "big"} onClick={selectLetter}>{keyVal}</div>
   )
 }
 
