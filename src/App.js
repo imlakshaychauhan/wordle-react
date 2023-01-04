5516e2dc7642a6066da2b9a127d6180cebac2ac4
imlakshaychauhan <imlakshaychauhan@gmail.com>
2023-01-04 19:01:59 +0530

    Board and Keyboard UI Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { createContext, useState } from 'react';
 import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+import { defaultBoard } from './Words';
 
-function App() {
+export const AppContext = createContext();
+
+const App = () => {
+  const [board, setBoard] = useState(defaultBoard);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav> <h1>WORDLE</h1> </nav>
+      <AppContext.Provider value={{board, setBoard}}>
+        <div className='game' >
+          <Board />
+          <Keyboard />
+        </div>
+      </AppContext.Provider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
